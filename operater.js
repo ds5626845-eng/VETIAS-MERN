4399c7b343c163eeb1259d2b3f5d180c9ad6f38a
ds5626845-eng <ds5626845@gmail.com>
2026-06-12 11:02:51 +0530

    Added operater.js

---
+++ operater.js
@@ -0,0 +1,24 @@
+//arithmetic
+var a=10
+var b=15
+console.log(a+b)
+//sub
+var a=15
+var b=23
+console.log(a-b)
+//multiplication
+var b=2
+var c=2
+console.log(b*c)
+//div
+var a=6
+var b=4
+console.log(a/b)
+//modulus
+var a=6
+var b=3
+console.log(a%b)
+//expe
+var a=4
+var b=6
+console.log(a**b)
