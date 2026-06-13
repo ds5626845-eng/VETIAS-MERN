5f1350393180b315f7bd46963f00185c4c3fda74
ds5626845-eng <ds5626845@gmail.com>
2026-06-13 14:02:59 +0530

    Added dom.html

--- conditional.js
+++ conditional.js
@@ -14,3 +14,20 @@ else if (light == "green") {
 else {
     console.log("Invalid signal");
 }
+
+console.log("the next eg:");
+var light = "Green";
+
+if (light == "Green") {
+    console.log("Go!");
+} 
+else if (light == "Yellow") {
+    console.log("Slow down!");
+} 
+else if (light == "Red") {
+    console.log("Stop!");
+} 
+else {
+    console.log("Invalid traffic light");
+}
+
